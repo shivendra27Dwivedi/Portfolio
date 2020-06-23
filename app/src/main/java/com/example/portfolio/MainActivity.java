98380dc9c99f1fe3f146f6f96ef7ca5cc2c32611
shivam dwivedi <shivamdwivedi618@gmail.com>
2020-06-23 13:00:56 +0530

    CONTACT

--- app/src/main/java/com/example/portfolio/MainActivity.java
+++ app/src/main/java/com/example/portfolio/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.portfolio;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
-import com.example.portfolio.home.HomeFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
-     private ViewPager viewPager;
+     private ImageView imageView1,imageView2,imageView3,imageView4,imageView5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getActionBar().hide();
-        setHomeFragment();
+        setContentView(R.layout.fragment_fragment_contact);
 
+      
     }
-    void setHomeFragment() {
 
-    }
+
 
 }
