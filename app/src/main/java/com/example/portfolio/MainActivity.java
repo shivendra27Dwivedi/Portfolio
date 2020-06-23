0acfd9b020e44f229c4a2a0c79053ac65b2624ca
shivam dwivedi <shivamdwivedi618@gmail.com>
2020-06-23 22:19:02 +0530

    CONTACT

--- app/src/main/java/com/example/portfolio/MainActivity.java
+++ app/src/main/java/com/example/portfolio/MainActivity.java
@@ -2,20 +2,105 @@ package com.example.portfolio;
 
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.portfolio.home.HomeFragment;
+import com.example.portfolio.home.fragmentContact;
+import com.example.portfolio.home.fragmentPortfolio;
+import com.example.portfolio.home.fragmentTeam;
+import com.example.portfolio.home.workFragment;
 
 
 public class MainActivity extends AppCompatActivity {
-     private ImageView imageView1,imageView2,imageView3,imageView4,imageView5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_fragment_contact);
+        setContentView(R.layout.activity_main);
 
+        // Create and set Android Fragment as default.
+        Fragment HomeFragment = new HomeFragment();
+        this.setDefaultFragment(HomeFragment);
+
+        // Click this button to display android fragment.
+        ImageView homeImage = findViewById(R.id.imageView);
+         homeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment HomeFragment = new HomeFragment();
+                replaceFragment(HomeFragment);
+            }
+        });
+
+        // Click this button to display windows fragment.
+        ImageView workImage = findViewById(R.id.imageView2);
+        workImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment workFragment = new workFragment();
+                replaceFragment(workFragment);
+            }
+        });
+
+        // Click this button to display iOS fragment.
+        ImageView portfolio = findViewById(R.id.imageView3);
+        portfolio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragmentPortfolio = new fragmentPortfolio();
+                replaceFragment(fragmentPortfolio);
+            }
+        });
+
+        // Click this button to display iOS fragment.
+        ImageView teamImage = findViewById(R.id.imageView4);
+        teamImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragmentTeam = new fragmentTeam();
+                replaceFragment(fragmentTeam);
+            }
+        });
+
+        // Click this button to display iOS fragment.
+        ImageView contactImage = findViewById(R.id.imageView5);
+        contactImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragmentContact = new fragmentContact();
+                replaceFragment(fragmentContact);
+            }
+        });
+
+    }
+
+    // This method is used to set the default fragment that will be shown.
+    private void setDefaultFragment(Fragment defaultFragment)
+    {
+        this.replaceFragment(defaultFragment);
+    }
+
+    // Replace current Fragment with the destination Fragment.
+    public void replaceFragment(Fragment destFragment)
+    {
+        // First get FragmentManager object.
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+
+        // Begin Fragment transaction.
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace the layout holder with the required Fragment object.
+        fragmentTransaction.replace(R.id.LinearLayout, destFragment);
+
+        // Commit the Fragment replace action.
+        fragmentTransaction.commit();
       
     }
 
